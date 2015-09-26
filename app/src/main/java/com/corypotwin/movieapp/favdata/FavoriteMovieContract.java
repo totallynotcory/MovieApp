c8238410cb8c5b2cf7a13fa8bd68010e14a13535
Cory Potwin <cpotwin@gmail.com>
2015-09-25 22:03:05 -0400

    database contract and dbhelper added.  tests not yet written

---
+++ app/src/main/java/com/corypotwin/movieapp/favdata/FavoriteMovieContract.java
@@ -0,0 +1,94 @@
+package com.corypotwin.movieapp.favdata;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+import android.text.format.Time;
+
+/**
+ * Defines table and column names for the weather database.
+ */
+public class FavoriteMovieContract {
+
+    // The "Content authority" is a name for the entire content provider, similar to the
+    // relationship between a domain name and its website.  A convenient string to use for the
+    // content authority is the package name for the app, which is guaranteed to be unique on the
+    // device.
+    public static final String CONTENT_AUTHORITY = "com.corypotwin.movieapp";
+
+    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+    // the content provider.
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Possible paths (appended to base content URI for possible URI's)
+    // For instance, content://com.corypotwin.movieapp/favorites
+    public static final String PATH_FAVORITES = "favorites";
+
+    // To make it easy to query for the exact date, we normalize all dates that go into
+    // the database to the start of the the Julian day at UTC.
+    public static long normalizeDate(long startDate) {
+        // normalize the start date to the beginning of the (UTC) day
+        Time time = new Time();
+        time.set(startDate);
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
+    }
+
+
+    /* Inner class that defines the table contents of the FavoriteMovie table */
+    public static final class FavoriteMovieEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAVORITES).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FAVORITES;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FAVORITES;
+
+        public static final String TABLE_NAME = "favoritemovies";
+
+        // Title of the movie.
+        public static final String COLUMN_TITLE = "title";
+        // Release date of the movie
+        public static final String COLUMN_DATE = "date";
+        // Movie rating
+        public static final String COLUMN_RATING = "rating";
+
+        // Description of the movie
+        public static final String COLUMN_DESC = "desc";
+
+        // URL/Location of the image
+        public static final String COLUMN_IMAGE = "image";
+
+        // URL/Location of the trailer
+        public static final String COLUMN_TRAILER = "trailer";
+
+        // Humidity is stored as a float representing percentage
+        public static final String COLUMN_REVIEWS = "reviews";
+
+        public static Uri buildFavoriteMoviesUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static String getTitleFromUri(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+
+        public static Uri buildTitleUri(String movieTitle) {
+            return CONTENT_URI.buildUpon().appendPath(movieTitle).build();
+        }
+
+        public static long getDateFromUri(Uri uri) {
+            return Long.parseLong(uri.getPathSegments().get(2));
+        }
+
+        public static long getRatingFromUri(Uri uri){
+            return Long.parseLong(uri.getPathSegments().get(3));
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/corypotwin/movieapp/favdata/FavoriteMovieDbHelper.java
@@ -0,0 +1,52 @@
+package com.corypotwin.movieapp.favdata;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.corypotwin.movieapp.favdata.FavoriteMovieContract.FavoriteMovieEntry;
+
+/**
+ * Class to construct the favorite movie database for users
+ */
+public class FavoriteMovieDbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    static final String DATABASE_NAME = "favoritemovies.db";
+
+    public FavoriteMovieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        final String SQL_CREATE_FAVORITEMOVIES_TABLE = "CREATE TABLE " + FavoriteMovieEntry.TABLE_NAME + " (" +
+                // Autoincrement is a questionable choice
+                // TODO do we really want an id that's auto-increment?
+                FavoriteMovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+
+                FavoriteMovieEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                FavoriteMovieEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
+                FavoriteMovieEntry.COLUMN_RATING + " REAL NOT NULL, " +
+                FavoriteMovieEntry.COLUMN_DESC + " TEXT NOT NULL, " +
+                FavoriteMovieEntry.COLUMN_IMAGE + " TEXT NOT NULL, " +
+                FavoriteMovieEntry.COLUMN_TRAILER + " TEXT NOT NULL, " +
+                FavoriteMovieEntry.COLUMN_REVIEWS + " TEXT NOT NULL, " +
+
+                // We want to make sure the same movie doesn't get added twice, but also need to
+                // make sure it's the same movie.  We're running out of titles for movies it seems
+                " UNIQUE (" + FavoriteMovieEntry.COLUMN_TITLE + ", " +
+                FavoriteMovieEntry.COLUMN_DATE + ") ON CONFLICT REPLACE);";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_FAVORITEMOVIES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        onCreate(sqLiteDatabase);
+    }
+
+
+}
