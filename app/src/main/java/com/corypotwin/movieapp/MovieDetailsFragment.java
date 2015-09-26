f664f03d64351ff7660874c6ccea0a6389eb5102
Cory Potwin <cpotwin@gmail.com>
2015-09-26 11:10:53 -0400

    detail page modified to include necessary fields

--- app/src/main/java/com/corypotwin/movieapp/MovieDetailsFragment.java
+++ app/src/main/java/com/corypotwin/movieapp/MovieDetailsFragment.java
@@ -1,14 +1,18 @@
 package com.corypotwin.movieapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.corypotwin.movieapp.favdata.FavoriteMovieContract;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -19,6 +23,7 @@ import java.util.ArrayList;
 public class MovieDetailsFragment extends Fragment {
 
     ArrayList<String> movieDetailsInHere;
+    private String movieTitle;
 
     public MovieDetailsFragment() {
     }
@@ -47,7 +52,8 @@ public class MovieDetailsFragment extends Fragment {
         movieDetailsInHere = intent.getStringArrayListExtra(Intent.EXTRA_TEXT);
 
         // Title
-        ((TextView) rootView.findViewById(R.id.title)).setText(movieDetailsInHere.get(0));
+        movieTitle = movieDetailsInHere.get(0);
+        ((TextView) rootView.findViewById(R.id.title)).setText(movieTitle);
 
         // Description
         if(movieDetailsInHere.get(1) == null){
@@ -85,5 +91,42 @@ public class MovieDetailsFragment extends Fragment {
                 .placeholder(R.drawable.no_image_available_black)
                 .error(R.drawable.no_image_available_black)
                 .into(imgView);
+
+        // Favorite
+        RatingBar favoriteStar = (RatingBar) rootView.findViewById(R.id.favorite_star);
+        if(setFavorite()){
+            favoriteStar.setRating(1);
+        }
     }
+
+    public boolean setFavorite(){
+        String[] selectionArgs = {movieTitle};
+
+        Cursor favoriteMovie = getActivity().getContentResolver()
+                .query(FavoriteMovieContract.FavoriteMovieEntry.CONTENT_URI,
+                        null,
+                        FavoriteMovieContract.FavoriteMovieEntry.COLUMN_TITLE,
+                        selectionArgs,
+                        null);
+
+        return favoriteMovie.moveToNext();
+    }
+
+    public void favoriteClick(RatingBar v){
+        if(v.getRating() == 1){
+            getActivity().getContentResolver()
+                    .delete(FavoriteMovieContract.FavoriteMovieEntry.CONTENT_URI,
+                            null,
+                            null);
+        } else {
+            //  TODO actually make these values so they get passed into the db
+            ContentValues values = new ContentValues();
+            values.put(FavoriteMovieContract.FavoriteMovieEntry.COLUMN_TITLE, movieTitle);
+            getActivity().getContentResolver()
+                    .insert(FavoriteMovieContract.FavoriteMovieEntry.CONTENT_URI,
+                            values);
+        }
+
+    }
+
 }
